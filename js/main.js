d7ecb8c2f341835f8a4931107708bcbb09677951
Alejandro <alejandroserna@iMac-de-Alejandro.local>
2020-10-05 20:31:41 -0500

    Menu Flotante Cliente

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-    /**Menu Transparente */
+$(document).ready(function () {
+  /**Menu Transparente */
   var nav = document.getElementById("nav");
-  window.onscroll = function() {
+  window.onscroll = function () {
     if (window.pageYOffset > 100) {
       nav.style.background = "rgba(8, 10, 114, 0.5)";
       nav.style.border = "none";
@@ -10,57 +10,94 @@ $(document).ready(function() {
       nav.style.borderBottom = "1px solid rgba(215, 215, 230, 0.5)";
     }
   };
-    /**Navegacion menu */
-  $('#home').on('click', function() {
+  /**Navegacion menu */
+  $("#home").on("click", function () {
     var position = 0;
-    $('body, html').animate( {
-        scrollTop: position + 'px'
-    }, 2000);
-    $('#home').addClass('linea-menu');
-    $('#planes').removeClass('linea-menu');
-    $('#servicios').removeClass('linea-menu');
-  })
+    $("body, html").animate(
+      {
+        scrollTop: position + "px",
+      },
+      2000
+    );
+    $("#home").addClass("linea-menu");
+    $("#planes").removeClass("linea-menu");
+    $("#servicios").removeClass("linea-menu");
+    $("#cliente").removeClass("linea-menu");
+  });
 
-  $('#planes').on('click', function() {
+  $("#planes").on("click", function () {
     var position = 575;
-    $('body, html').animate( {
-        scrollTop: position + 'px'
-    }, 2000);
-    $('#planes').addClass('linea-menu');
-    $('#home').removeClass('linea-menu');
-     $('#servicios').removeClass('linea-menu');
-  })
+    $("body, html").animate(
+      {
+        scrollTop: position + "px",
+      },
+      2000
+    );
+    $("#planes").addClass("linea-menu");
+    $("#home").removeClass("linea-menu");
+    $("#servicios").removeClass("linea-menu");
+    $("#cliente").removeClass("linea-menu");
+  });
 
-  $('#servicios').on('click', function() {
+  $("#servicios").on("click", function () {
     var position = 1500;
-    $('body, html').animate( {
-        scrollTop: position + 'px'
-    }, 2000);
-    $('#servicios').addClass('linea-menu');
-    $('#home').removeClass('linea-menu');
+    $("body, html").animate(
+      {
+        scrollTop: position + "px",
+      },
+      2000
+    );
+    $("#servicios").addClass("linea-menu");
+    $("#home").removeClass("linea-menu");
+    $("#planes").removeClass("linea-menu");
+    $("#cliente").removeClass("linea-menu");
+  });
+  /**$('#cliente').on('mouseenter mouseleave', function() {  
+    $('.sub-menu').addClass('show-menu');
+    $('#home').removeClass('sub-menu');
     $('#planes').removeClass('linea-menu');
-  })
-   
-  /**Clck en planes Hogar - Negocio */
-  $('.i-hogar').on('click', function() {
-    var position = 880;
-    $('body, html').animate( {
-      scrollTop: position + 'px'
-    }, 2000);
-    $('.hogar').addClass('show-hogar');
-    $('.negocio').removeClass('show-negocio');
-    $('.i-hogar').addClass('i-f-hogar');
-    $('.i-negocio').removeClass('i-f-negocio');
-  })
+  })**/
 
-  $('.i-negocio').on('click', function() {
+  //$("#cliente").click(function (e) {
+  $("#cliente").on("click", function () {
+    // Resetear, por si acaso has estado jugando con la otra propiedad
+    $(".sub-menu").css("visibility", "visible");
+
+    $(".sub-menu").css("left", "420px");
+
+    if ($(".sub-menu").is(":visible")) {
+      $(".sub-menu").css("display", "none");
+    } else {
+      $(".sub-menu").css("display", "block");
+    }
+  });
+
+  /**Clck en planes Hogar - Negocio */
+  $(".i-hogar").on("click", function () {
     var position = 880;
-    $('body, html').animate( {
-      scrollTop: position + 'px'
-    }, 2000);
-    $('.negocio').addClass('show-negocio');
-    $('.hogar').removeClass('show-hogar');
-    $('.i-negocio').addClass('i-f-negocio');
-    $('.i-hogar').removeClass('i-f-hogar');
-  })
+    $("body, html").animate(
+      {
+        scrollTop: position + "px",
+      },
+      2000
+    );
+    $(".hogar").addClass("show-hogar");
+    $(".negocio").removeClass("show-negocio");
+    $(".i-hogar").addClass("i-f-hogar");
+    $(".i-negocio").removeClass("i-f-negocio");
+  });
+
+  $(".i-negocio").on("click", function () {
+    var position = 880;
+    $("body, html").animate(
+      {
+        scrollTop: position + "px",
+      },
+      2000
+    );
+    $(".negocio").addClass("show-negocio");
+    $(".hogar").removeClass("show-hogar");
+    $(".i-negocio").addClass("i-f-negocio");
+    $(".i-hogar").removeClass("i-f-hogar");
+  });
 });
